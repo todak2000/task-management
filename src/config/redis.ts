970878e7c0bc3c69b9c6eb2992b38110953b41a4
Daniel Olagunju <todak2000@gmail.com>
2025-03-14 10:17:20 +0100

    using prod redis

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,10 +1,10 @@
-// config/redis.ts
 import Redis from "ioredis";
 
 const redisClient = new Redis({
-  host: process.env.REDIS_HOST || "localhost",
-  port: parseInt(process.env.REDIS_PORT || "6379"),
-  password: process.env.REDIS_PASSWORD,
+  username: `${process.env.REDIS_USERNAME}`, // Redis username
+  password: `${process.env.REDIS_PASSWORD}`, // Redis password
+  host: `${process.env.REDIS_HOST}` || "localhost", // Redis host
+  port: parseInt(`${process.env.REDIS_PORT}` || "6379"), // Redis port
 });
 
 export default redisClient;
